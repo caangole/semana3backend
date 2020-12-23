497c09aad731b8d4e90169a1d84661b1b0534ac8
caangole <caangole@gmail.com>
2020-12-23 18:28:18 -0500

    modificación en los CORS

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const controller = require('./controllers/UserController');
 //Instancia de Express
 const app = express();
 app.use(cors());
-//   app.use((req, res, next) =>{
-//      res.header("Access-Control-Allow-Origin", "*");
-//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//      res.header("Access-Control-Allow-Methods: GET, POST, DELETE");
-//      next();
-//  })
+  app.use((req, res, next) =>{
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.header("Access-Control-Allow-Methods: GET, POST, DELETE");
+     next();
+ })
 
 
 //middleware Morgan para detectar peticiones
